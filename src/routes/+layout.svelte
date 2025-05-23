472653b6701928603d970f5dab0414e37d25e40f
devcomfort <im@devcomfort.me>
2025-05-24 02:23:00 +0900

    feat(layout): structure main application layout using +layout.svelte
    - `routes/+layout.svelte`: Defines the primary application screen layout by arranging `Header`, `Sidebar`, and the main content `<slot />`. Establishes the overall visual structure. Includes TODOs for dynamic resizing and footer addition.

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import Header from '$lib/components/Header.svelte';
+	import Sidebar from '$lib/components/Sidebar.svelte';
+	import '../tailwind.css';
+</script>
+
+<div class="flex h-screen w-screen flex-col">
+	<!-- header -->
+	<Header />
+
+	<!-- main content -->
+	<!-- TODO: <slot /> 와 Sidebar 컴포넌트의 영역 크기가 자동으로 조정되도록 해야함 -->
+	<Sidebar />
+	<slot />
+
+	<!-- TODO: footer 추가 필요 -->
+</div>
