a8d79489f52c72ac44d11d656508dcb4722ac277
devcomfort <im@devcomfort.me>
2025-05-26 16:14:38 +0900

    config: update Tailwind CSS setup and DaisyUI integration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,6 @@
 import type { Config } from "tailwindcss";
+import typography from "@tailwindcss/typography";
+import daisyui from "daisyui";
 
 export default {
 	content: ["./src/**/*.{html,js,svelte,ts}"],
@@ -11,5 +13,5 @@ export default {
 		},
 	},
 
-	plugins: [require("@tailwindcss/typography")],
+	plugins: [typography(), daisyui()],
 } as Config;
